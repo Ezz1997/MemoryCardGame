d291f6e69810e6aac775155dcc0e6bce53ac925d
Ezz1997 <ezmarie97@gmail.com>
2025-02-27 09:26:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,133 @@
+import { useState, useEffect } from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import Images from "./Images";
+import "./App.css";
+import BlueCard from "./assets/blue-card.png";
+import Timer from "./components/Timer";
+
+const UNIQUE_IMAGES_NUM = 6;
+
+export default function App() {
+  const [cards, setCards] = useState(new Map());
+  const [images, setImages] = useState([]);
+  const [imageBuffer, setImageBuffer] = useState(new Map());
+  const [isDone, setIsDone] = useState(new Set());
+  const [timer, setTimer] = useState("00:20");
+  
+  function updateImages() {
+    let temp_images = structuredClone(Images);
+    shuffleImages(temp_images);
+    temp_images = structuredClone(temp_images.slice(0, UNIQUE_IMAGES_NUM));
+    const newImages = temp_images.concat(structuredClone(temp_images));
+    shuffleImages(newImages);
+
+    setImages(newImages);
+  }
+
+  useEffect(() => {
+    updateImages();
+  }, []);
+
+  // Fisher-Yates shuffle
+  function shuffleImages(images) {
+    for (let i = images.length - 1; i >= 1; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [images[i], images[j]] = [images[j], images[i]];
+    }
+  }
+
+  function handleFlip(id, imageURL) {
+    const newMap = new Map(cards);
+    let bufferMap = new Map(imageBuffer);
+    const isFlipped = cards.get(id);
+
+    // make sure no more than 2 images are flipped at a time
+    if (!isFlipped && bufferMap.size === 2) return;
+
+    newMap.set(id, isFlipped !== undefined ? !isFlipped : true);
+    setCards(newMap);
+
+    if (bufferMap.has(id)) {
+      bufferMap.delete(id);
+    } else {
+      bufferMap.set(id, imageURL);
+    }
+
+    if (bufferMap.size === 2) {
+      const bufferSet = new Set(bufferMap.values());
+      if (bufferSet.size === 1) {
+        setIsDone(new Set([...isDone, ...bufferSet]));
+        bufferMap = new Map();
+      } else {
+        console.log("Different Images");
+      }
+    }
+
+    setImageBuffer(bufferMap);
+  }
+
+  useEffect(() => {
+    console.log(isDone);
+  }, [isDone]);
+
+  // resetting game params on new game
+  function resetParams() {
+    setCards(new Map());
+    setImages([]);
+    setImageBuffer(new Map());
+    setIsDone(new Set());
+    setTimer("00:20");
+
+    updateImages();
+  }
+
+  return (
+    <div className="center-element">
+      <Stack
+        spacing={2}
+        sx={{ justifyContent: "center", alignItems: "center" }}
+      >
+        <ImageList sx={{ width: 600, height: 520}} cols={4} rowHeight={164}>
+          {images.map((item, idx) => (
+            <ImageListItem key={idx}>
+              <img
+                srcSet={
+                  cards.get(idx)
+                    ? `${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`
+                    : `${BlueCard}`
+                }
+                src={
+                  cards.get(idx)
+                    ? `${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`
+                    : `${BlueCard}`
+                }
+                alt={item.title}
+                loading="lazy"
+                onClick={
+                  isDone.has(item.img) || timer === "00:00" ? null : () => handleFlip(idx, item.img)
+                }
+                className={`flip-card ${cards.get(idx) ? "back" : "front"} ${isDone.has(item.img) ? "highlight-correct" : ""}`}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+        <Button
+          sx={{ width: "30%" }}
+          variant="contained"
+          size="medium"
+          onClick={resetParams}
+          disabled={!(isDone.size === UNIQUE_IMAGES_NUM || timer === "00:00")}
+        >
+          Try Again
+        </Button>
+        {isDone.size === UNIQUE_IMAGES_NUM && <Alert>You Won!</Alert>}
+        {(timer === "00:00" && isDone.size !== UNIQUE_IMAGES_NUM) && <Alert severity="error">Game Over!</Alert>}
+        <Timer timer={timer} setTimer={setTimer} isGameOver={isDone.size === UNIQUE_IMAGES_NUM || timer === "00:00"}/>
+      </Stack>
+    </div>
+  );
+}
---
+++ src/components/Timer.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const Timer = ({timer, setTimer, isGameOver}) => {
+    // We need ref in this, because we are dealing
+    // with JS setInterval to keep track of it and
+    // stop it when needed
+    const Ref = useRef(null);
+
+    // The state for our timer
+    // const [timer, setTimer] = useState("00:20");
+
+    const getTimeRemaining = (e) => {
+        const total =
+            Date.parse(e) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor(
+            (total / 1000 / 60) % 60
+        );
+        return {
+            total,
+            minutes,
+            seconds,
+        };
+    };
+
+    const startTimer = (e) => {
+        let { total, minutes, seconds } =
+            getTimeRemaining(e);
+        if (total >= 0) {
+            // update the timer
+            // check if less than 10 then we need to
+            // add '0' at the beginning of the variable
+            setTimer(
+                (minutes > 9
+                    ? minutes
+                    : "0" + minutes) +
+                ":" +
+                (seconds > 9 ? seconds : "0" + seconds)
+            );
+        }
+    };
+
+    const clearTimer = (e) => {
+        // If you adjust it you should also need to
+        // adjust the Endtime formula we are about
+        // to code next
+        setTimer("00:20");
+
+        // If you try to remove this line the
+        // updating of timer Variable will be
+        // after 1000ms or 1sec
+        if (Ref.current) clearInterval(Ref.current);
+        let id = setInterval(() => {
+            startTimer(e);
+        }, 1000);
+        Ref.current = id;
+    };
+
+    const getDeadTime = () => {
+        let deadline = new Date();
+
+        // This is where you need to adjust if
+        // you intend to add more time
+        deadline.setSeconds(deadline.getSeconds() + 20);
+        return deadline;
+    };
+
+    useEffect(() => {
+            isGameOver ? clearInterval(Ref.current) : clearTimer(getDeadTime());
+        }, [isGameOver]);
+
+    return (
+        <div
+            style={{ textAlign: "center", margin: "auto" }}>
+            <h2>{timer}</h2>
+        </div>
+    );
+};
+
+export default Timer;
